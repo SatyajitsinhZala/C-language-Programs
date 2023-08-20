55d67fe797a26894651d54650034ffd20676fa39
SatyajitsinhZala <142555890+SatyajitsinhZala@users.noreply.github.com>
2023-08-20 12:36:26 +0530

    3. Sum of two numbers
    An program to Calculate sum of two numbers given by user.

---
+++ 3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,sum;
+    printf("Enter 1 number");
+    scanf("%d",&a);
+    printf("Enter 2 number");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("Sum=%d",sum);
+}
