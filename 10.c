222c6c887e62fd6b46700bbd010290729abcc76c
SatyajitsinhZala <142555890+SatyajitsinhZala@users.noreply.github.com>
2023-09-01 09:19:58 +0530

    Add files via upload

--- 10.c
+++ 10.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 void main()
 {
-    int y,w,d;
-    printf("Enter no of Days\n");
-    scanf("%d",&d);
-    y=d/365;
-    w=d/7;
-    printf("%d,%d,%d",y,w,d);
+	int y,d,w;
+	printf("Enter no of days");
+	scanf("%d",&d);
+	y=d/365;
+	d=d%365;
+	w=d/7;
+	d=d%7;
+	printf("%d,%d,%d",y,d,w);	
+	
 }
